2591684196a8851b9d836416361f44693d5091d6
smith-sj <mr.sjsmith@protonmail.com>
2022-08-29 12:04:30 +1000

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('hi')
+
+const mobileNav = document.querySelectorAll('.nav-link');
+const checkBox = document.getElementById('mobile-menu-toggle');
+
+checkBox.addEventListener('change', (event) => {
+    console.log("hi")
+    if (event.currentTarget.checked) {
+        for (i = 0; i < mobileNav.length; ++i) {
+            mobileNav[i].style.display = "block"
+            document.querySelector(".mobile-main-nav").style.top = "0"
+        }
+    } else {
+        for (i = 0; i < mobileNav.length; ++i) {
+            document.querySelector(".mobile-main-nav").style.top = "-300%"
+            // mobileNav[i].style.display = "none"
+        }
+    }
+});
