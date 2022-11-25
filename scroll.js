f36eac191b1bc55defe6e8c1f4537389c8b55c97
smith-sj <mr.sjsmith@protonmail.com>
2022-11-26 00:53:32 +1100

    scroll effect in media query

--- scroll.js
+++ scroll.js
@@ -3,30 +3,15 @@ const projects = document.querySelectorAll(".project-container");
 
 function handleScroll() {
     projects.forEach((project) => {
-        if (window.innerWidth < 560) {
             let projectRect = project.getBoundingClientRect();
             if (projectRect.y < window.innerHeight * 0.6 && projectRect.y > window.innerHeight * 0.15) {
                 project.classList.add("scrolled-project");
             } else {
                 project.classList.remove("scrolled-project");
             }
-        } else {
-                        projects.forEach((project) => {
-                project.classList.remove("scrolled-project");
-            });
-        }
+        
     });
 }
 
 
 window.addEventListener('scroll', handleScroll)
-// window.addEventListener(resize, () => {
-//     if (window.innerWidth < 500) {
-//         window.addEventListener("scroll", handleScroll);
-//     } else {
-//         window.removeEventListener("scroll", handleScroll);
-//         projects.forEach((project) => {
-//             project.classList.remove("scrolled-project");
-//         });
-//     }
-// })
